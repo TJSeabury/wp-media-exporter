896a0631abab1a9d37b9141cc73c59d126171848
TJSeabury <tylerseabury@gmail.com>
2022-05-02 12:21:02 -0400

    got image data fetching working

--- index.js
+++ index.js
@@ -18,22 +18,30 @@ const xml2js = require( 'xml2js' );
 
 const parser = new xml2js.Parser(/* options */ );
 
-//const mediaPath = path.resolve( __dirname, 'sweitzerconstruction.WordPress.2022-05-02.xml' );
-const mediaPath = path.resolve( __dirname, 'test.xml' );
+const mediaPath = path.resolve( __dirname, 'sweitzerconstruction.WordPress.2022-05-02.xml' );
+//const mediaPath = path.resolve( __dirname, 'test.xml' );
 const mediaXml = fs.readFileSync( mediaPath );
 
 parser.parseString( mediaXml, async function ( err, result ) {
     for ( const item of result.rss.channel[0].item ) {
         const url = ( item?.['wp:attachment_url']?.[0] ).trim();
 
-        const image = await fetch( url );
+        const image = await ( await ( await fetch( url ) ).blob() ).arrayBuffer();
 
-        saveWithRelativePath( url, image );
+        saveWithRelativePath( url, toBuffer( image ) );
 
     }
     console.log( 'Done' );
 } );
 
+function toBuffer ( ab ) {
+    const buf = Buffer.alloc( ab.byteLength );
+    const view = new Uint8Array( ab );
+    for ( let i = 0; i < buf.length; ++i ) {
+        buf[i] = view[i];
+    }
+    return buf;
+}
 
 function schemaFromUrl ( path ) {
     const reg = /^https?:\/\/w{3}?\.?(\w+?\.com)\/([\w\-\/]+?)\/([\w\-]+.(?:jpg|jpeg|png|tif|webp))$/g;
@@ -54,7 +62,7 @@ function saveWithRelativePath ( url, data ) {
     const schema = schemaFromUrl( url );
     const domainDir = path.resolve( __dirname, schema?.domain );
     if ( !fs.existsSync( domainDir ) ) fs.mkdirSync( domainDir );
-    let wd = __dirname;
+    let wd = domainDir;
     for ( const dir of schema?.dirPath ) {
         const currentDir = path.resolve( wd, dir );
         if ( !fs.existsSync( currentDir ) ) fs.mkdirSync( currentDir );
